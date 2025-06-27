665fcbc5a055986f300eb291cf45962f0ba8ca74
merrrrrick <merrickpereira@gmail.com>
2025-06-28 00:17:43 +0530

    initial commit

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/make-notes";
+const mongoURI = "mongodb+srv://merrickpereira:Merrick12@notes-cluster.ansquow.mongodb.net/";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI).then(()=>console.log("Connected to MongoDB")).catch((e)=>console.log(e.message));

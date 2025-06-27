0ac0b3dbcd3e11ce10a5c7d5a8870963fc8844d0
merrrrrick <merrickpereira@gmail.com>
2025-06-28 00:49:56 +0530

    initial commit

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -10,7 +10,7 @@ const Signup = (props) => {
 
     const handleSubmit = async (e) => {
         e.preventDefault(); // not to reload page on clicking bbutton
-        const response = await fetch('http://localhost:5000/api/auth/createuser', {
+        const response = await fetch('https://make-notes-72a5.onrender.com/api/auth/createuser', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
